e541e2179a0de99984e8bdf852bf64ca460c0c41
Sammani Ariyarathne <sammani.ari@gmail.com>
2025-04-28 23:49:03 +0200

    added header styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
 
 const Header = () => {
-	return (
-    <>
-      <a href="#home">Home</a>
-      <a href="#our-story">Our Story</a>
-      <a href="#venue">Venue</a>
-      <a href="#itinierary">Itinierary</a>
-      <a href="#registry">Registry</a>
-      <a href="#rsvp">RSVP</a>
-      <a href="#footer">Footer</a>
-    </>
+  return (
+    <div className="header">
+      <div className="nav">
+        <a href="#home">Home</a>
+        <a href="#our-story">Our Story</a>
+        <a href="#venue">Venue</a>
+        <img
+          src="/navLogo.svg"
+          alt="C and A Logo"
+          className="logo"
+        />
+        <a href="#itinierary">Itinierary</a>
+        <a href="#registry">Registry</a>
+        <a href="#rsvp">RSVP</a>
+        <a href="#footer">Footer</a>
+      </div>
+    </div>
   );
-}
+};
 
 export default Header
