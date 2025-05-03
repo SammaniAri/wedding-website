33b86e487dfdc8c6b269b8aa07cfa795ca888ed1
Sammani Ariyarathne <sammani.ari@gmail.com>
2025-05-03 16:42:58 +0200

    our-story image

--- src/components/OurStory.jsx
+++ src/components/OurStory.jsx
@@ -2,34 +2,41 @@ import React from 'react'
 
 const OurStory = () => {
 	return (
-    <div
-      id="our-story"
-      className="section"
-    >
-      <div className="our-story-title">
-        <p>Our</p>
-        <p>Story</p>
+    <div>
+      <div
+        id="our-story"
+        className="section"
+      >
+        <div className="our-story-title">
+          <p>Our</p>
+          <p>Story</p>
+        </div>
+        <div className="our-story-text">
+          <p>
+            Feugiat pretium egestas enim blandit purus euismod. Feugiat magna
+            aliquam lectus lectus eu amet. Eros, accumsan purus enim nascetur
+            quam diam felis, fringilla varius. Quis purus nisl orci eu,
+            ultrices. Purus pretium egestas ultricies tempus sit elit. Maecenas
+            pellentesque sit eros vitae. Maecenas suspendisse tincidunt
+            ullamcorper justo neque quis et, laoreet.
+          </p>
+          <p>
+            Vitae lacus, aliquet lorem mauris, sit dolor sodales. Nullam quam
+            quis lorem dui tristique massa enim. Faucibus sed egestas mollis
+            vivamus et sed sed.
+          </p>
+          <p>
+            Feugiat magna aliquam lectus lectus eu amet. Eros, accumsan purus
+            enim nascetur quam diam felis, fringilla varius. Quis purus nisl
+            orci eu, ultrices. Purus pretium egestas ultricies tempus sit elit.
+          </p>
+        </div>
       </div>
-      <div className="our-story-text">
-        <p>
-          Feugiat pretium egestas enim blandit purus euismod. Feugiat magna
-          aliquam lectus lectus eu amet. Eros, accumsan purus enim nascetur quam
-          diam felis, fringilla varius. Quis purus nisl orci eu, ultrices. Purus
-          pretium egestas ultricies tempus sit elit. Maecenas pellentesque sit
-          eros vitae. Maecenas suspendisse tincidunt ullamcorper justo neque
-          quis et, laoreet.
-        </p>
-        <p>
-          Vitae lacus, aliquet lorem mauris, sit dolor sodales. Nullam quam quis
-          lorem dui tristique massa enim. Faucibus sed egestas mollis vivamus et
-          sed sed.
-        </p>
-        <p>
-          Feugiat magna aliquam lectus lectus eu amet. Eros, accumsan purus enim
-          nascetur quam diam felis, fringilla varius. Quis purus nisl orci eu,
-          ultrices. Purus pretium egestas ultricies tempus sit elit.
-        </p>
-      </div>
+      <img
+        src="/our-story-image.jpg"
+        alt="image of Caleb and Amaya"
+        className="our-story-image"
+      />
     </div>
   );
 }
