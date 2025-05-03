4d4d783343e1d082bed740647f5ca083d0a47735
Sammani Ariyarathne <sammani.ari@gmail.com>
2025-05-03 19:36:32 +0200

    venue-info text sectin

--- src/components/VenueInfo.jsx
+++ src/components/VenueInfo.jsx
@@ -6,7 +6,18 @@ const VenueInfo = () => {
       id="venue"
       className="section"
     >
-      VenueInfo
+      <div className="venue-text">
+        <p className="venue-section-name">The Venue</p>
+        <p className="venue-title">Gino Winery Estate</p>
+        <p>
+          Feugiat pretium egestas enim blandit purus euismod. Feugiat magna
+          aliquam lectus lectus eu amet. Eros, accumsan purus enim nascetur quam
+          diam felis, fringilla varius.
+        </p>
+        <p>4750 Tyler Avenue</p>
+        <p>Boise, Idaho 83705</p>
+      </div>
+      <div className="venue-carousel"></div>
     </div>
   );
 }
