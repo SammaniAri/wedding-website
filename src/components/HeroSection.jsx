19726d7eff6bc19eb6d8cb92a7b5e104e36a08cc
Sammani Ariyarathne <sammani.ari@gmail.com>
2025-05-03 16:04:33 +0200

    our-story section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,14 +2,11 @@ import React from 'react'
 
 const HeroSection = () => {
 	return (
-    <div
-      id="home"
-      className="section"
-    >
+    <div id="home">
       <div className="hero-text">
-        <p className="subheading">Please join us to celebrate</p>
+        <p>Please join us to celebrate</p>
         <h1>Caleb & Amaya</h1>
-        <p className="subheading">July 15, 2022 - Boise, Idaho</p>
+        <p>July 15, 2022 - Boise, Idaho</p>
       </div>
       <div>
         <img
--- src/components/OurStory.jsx
+++ src/components/OurStory.jsx
@@ -1,7 +1,37 @@
 import React from 'react'
 
 const OurStory = () => {
-	return <div id="our-story" className='section'>OurStory</div>;
+	return (
+    <div
+      id="our-story"
+      className="section"
+    >
+      <div className="our-story-title">
+        <p>Our</p>
+        <p>Story</p>
+      </div>
+      <div className="our-story-text">
+        <p>
+          Feugiat pretium egestas enim blandit purus euismod. Feugiat magna
+          aliquam lectus lectus eu amet. Eros, accumsan purus enim nascetur quam
+          diam felis, fringilla varius. Quis purus nisl orci eu, ultrices. Purus
+          pretium egestas ultricies tempus sit elit. Maecenas pellentesque sit
+          eros vitae. Maecenas suspendisse tincidunt ullamcorper justo neque
+          quis et, laoreet.
+        </p>
+        <p>
+          Vitae lacus, aliquet lorem mauris, sit dolor sodales. Nullam quam quis
+          lorem dui tristique massa enim. Faucibus sed egestas mollis vivamus et
+          sed sed.
+        </p>
+        <p>
+          Feugiat magna aliquam lectus lectus eu amet. Eros, accumsan purus enim
+          nascetur quam diam felis, fringilla varius. Quis purus nisl orci eu,
+          ultrices. Purus pretium egestas ultricies tempus sit elit.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default OurStory
